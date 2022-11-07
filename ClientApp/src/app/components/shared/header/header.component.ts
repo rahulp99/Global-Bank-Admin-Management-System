8172f63508b1519831108ebce94ca22239673fad
Rishabh <r@k.com>
2022-11-08 01:31:57 +0530

    Adding Header,Footer2

---
+++ ClientApp/src/app/components/shared/header/header.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminLoginService } from 'src/app/services/admin-login.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  navToggle: Boolean = false;
+  constructor(private adminLoginService: AdminLoginService) { }
+
+  ngOnInit(): void {
+  }
+
+  logOut() {
+    this.adminLoginService.logOut();
+  }
+
+  toggleNav() {
+    this.navToggle  = !this.navToggle;
+  }
+
+}
