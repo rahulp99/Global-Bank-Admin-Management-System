0048581f5e54fc14f2b370fc50684b2d0888c31c
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-11-08 14:12:34 +0530

    uncommented authentication

--- Program.cs
+++ Program.cs
@@ -24,20 +24,20 @@ namespace Global_Bank_Admin_Management_System
 
             builder.Services.AddDbContext<AdminContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConnection")));
 
-            //builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
-            //{
-            //    x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-            //    {
-            //        ValidateIssuer = true,
-            //        ValidateAudience = true,
-            //        ValidateLifetime = true,
-            //        ValidateIssuerSigningKey = true,
-            //        ValidIssuer = "localhost",
-            //        ValidAudience = "localhost",
-            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwtConfig:Key"])),
-            //        ClockSkew = TimeSpan.Zero
-            //    };
-            //});
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+            {
+                x.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "localhost",
+                    ValidAudience = "localhost",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["jwtConfig:Key"])),
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
 
 
             builder.Services.AddControllersWithViews();
@@ -64,7 +64,7 @@ namespace Global_Bank_Admin_Management_System
             app.UseRouting();
             app.UseCors("AllowOrigin");
 
-            //app.UseAuthentication();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapControllerRoute(
